396561e8761db4957decb6531a94f66b37badbdc
Joel Meyerson <jrmeyerson@gmail.com>
2021-07-18 21:20:30 -0400

    batch prediction implemented

--- gen_grid.py
+++ gen_grid.py
@@ -3,6 +3,7 @@ import shutil
 import argparse
 import sys
 import re
+import pathlib
 import math
 import numpy as np
 import mrcfile
@@ -12,12 +13,12 @@ from progress.bar import Bar
 # local imports
 from support_modules import extract_star_meta
 
-def gen_grid_star():
+def gen_grid():
     
     # parse arguments
     parser = argparse.ArgumentParser(description='Extract segments (boxes) from images in a grid pattern using sliding window.')
     parser.add_argument('-p', '--projpath', type=str, help='path for project', required=True)
-    parser.add_argument('-i', '--imagestar', type=str, help='micograph.star or micrograph_ctf.star for the target micrographs)', required=True)
+    parser.add_argument('-i', '--imagestar', type=str, help='micrograph.star for the target micrographs)', required=True)
     #parser.add_argument('-m', '--metastar', type=str, help='any particle star file containing the target box size', required=True)
     parser.add_argument('-x', '--boxsize', type=int, help='box size (must match box size used for training/testing.)', required=True)
     args = parser.parse_args()
@@ -28,27 +29,14 @@ def gen_grid_star():
         work_dir = work_dir.rstrip('/')
         
     data_dir = args.projpath + '/ClassBin'
-    micro_dir = args.projpath + '/Micrographs'
-    
-    if (os.path.exists(micro_dir) == False):
-        print("\nMicrographs directory not found, but needed to store star file output. Exiting.")
-        exit()
-        
-    if (os.path.exists(micro_dir) == True):
-        print("\nMicrographs directory found. Confirm if _grid.star files can be written to Micrographs directory (y/n)")
-        yn = input()
-        if yn == 'n':
-            print("\nExiting without writing _grid.star files.")
-        elif yn == 'y':
-            pass
-        else:
-            print("\nInvalid selection. Exiting")
-            exit()
         
     # get box size
     #meta = extract_star_meta.extract(args.metastar)
     #box = int(meta[0])
     box = args.boxsize
+    
+    # used to later store micrograph path name from star file
+    micro_dir = ""
     
     # extract name of image stack and index for image slice
     files = [] # store files in array
@@ -64,21 +52,40 @@ def gen_grid_star():
                 path = work_dir + "/" + column_str
                 img = str(path.split("/")[-1])
                 img = img.split(".")[0]
-
+                
+                # if micro_dir variable is empty, get path to micrograph name
+                if micro_dir == "":
+                    micro_dir = work_dir + "/" + str(pathlib.Path(column_str).parent)
+                    
                 # store micrograph path and micrograph name
                 files.append([path, img])
     
+    if (os.path.exists(micro_dir) == False):
+        print("\nMicrographs directory not found, but needed to store star file output. Exiting.")
+        exit()
+
+    if (os.path.exists(micro_dir) == True):
+        print("\nMicrographs directory found. Confirm if _grid.star files can be written to " + micro_dir + " path. (y/n)")
+        yn = input()
+        if yn == 'n':
+            print("\nExiting without writing _grid.star files.")
+        elif yn == 'y':
+            pass
+        else:
+            print("\nInvalid selection. Exiting")
+            exit()
+
     # convert array to tensor
     files = np.array(files)
     num_file = int(len(files))
-    
+
     # read mrc header
     micrograph = mrcfile.open(files[0,0], mode=u'r', permissive=False, header_only=True);
     header = micrograph.header
     dimx = header.nx
     dimy = header.ny
     dimz = header.nz
-    
+
     if dimz > 1:
         print("\nSTAR file must contain flat images not movie stacks. Exiting.")
         exit()
@@ -95,24 +102,24 @@ def gen_grid_star():
     lines.append('_rlnClassNumber #3')
     lines.append('_rlnAnglePsi #4')
     lines.append('_rlnAutopickFigureOfMerit #5')
-    
+
     # number of samples along y and x
     boxes_y = math.floor(dimy/box)
     boxes_x = math.floor(dimx/box)
-    
+
     # generate a star file entry for each coordinate in the grid
     for y in range(1, boxes_y+1):
         for x in range(1, boxes_x+1):
             lines.append(str(format((box*x)-(box/2), '.1f').rjust(10)) + ' ' + str(format((box*y)-(box/2), '.1f').rjust(10)) + '            2   -999.00000   -999.00000')
             if (x < boxes_x or dimx % box > 0.5) : # decide whether or not to add the final coordinate in a super sample row
                 lines.append(str(format((box*x), '.1f').rjust(10)) + ' ' + str(format((box*y), '.1f').rjust(10)) + '            2   -999.00000   -999.00000')
-    
+
     # write coordinate star file for each micrograph
     for f in files:
         with open(micro_dir + '/' + f[1] + "_grid.star", "w") as starfile:
            starfile.writelines("%s\n" % l for l in lines)
- 
-    print("\nSuccessfully generated _grid.star files in Micrographs directory.")
+
+    print("\nSuccessfully generated _grid.star files in " + micro_dir + " directory.")
     
 if __name__ == "__main__":
-    gen_grid_star()
+    gen_grid()
--- gen_picks.py
+++ gen_picks.py
@@ -1,17 +1,18 @@
 import os
 import shutil
 import argparse
+import sys
+import re
+import math
 import numpy as np
+import mrcfile
+from PIL import Image
 import matplotlib.pyplot as plt
+from progress.bar import Bar
 
 # TF imports
 import tensorflow as tf
 from tensorflow import keras
-
-# local imports
-# import extract_star_meta
-# import extract_particles
-# import save_results
 
 # disable GPU
 # os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
@@ -22,7 +23,7 @@ def gen_picks():
     # parse arguments
     parser = argparse.ArgumentParser(description='Use model to classify extracted images as good (true particles) or bad (empty area, ice contamination, gold/carbon film).')
     parser.add_argument('-p', '--projpath', type=str, help='path for project', required=True)
-    parser.add_argument('-i', '--imagestar', type=str, help='particle star file containing inputs for classification using model', required=True)
+    parser.add_argument('-i', '--imagestar', type=str, help='particle star file containing extracted grid particles for classification using model', required=True)
     args = parser.parse_args()
     
     # format project path 
@@ -33,211 +34,135 @@ def gen_picks():
     # check that sub-directory exists
     data_dir = work_dir + '/ClassBin'
     if os.path.exists(data_dir) == False:
-        "No ClassBin directory found."
+        print("No ClassBin directory found.")
         exit()
     
     # check that model exists
     if os.path.exists(data_dir + '/model.h5') == False:
-        "No model file found (h5)."
+        print("No model file (h5 format) found.")
         exit()
-        
-    # extract box size, box apix, and original image apix
-    meta = extract_star_meta.extract(args.inputstar)
-    print("Box size: " + meta[0] + ", Box apix: " + meta[1] + ", Image apix: " + meta[2])
-    box = int(meta_good[0])
-    b_apix = float(meta_good[1])
-    i_apix = float(meta_good[2])
+
+    model = tf.keras.models.load_model(data_dir + '/model.h5')
+    # load model
+    try:
+        model = tf.keras.models.load_model(data_dir + '/model.h5')
+    except:
+        print("Error loading model. Exiting")
+        exit()
+
     
-    # create directories to store training/test data
-    if (len(os.listdir(data_dir)) == 0 or args.cleardata == True):
-        
-        # FOR DEBUGGING
-        if args.cleardata == True:
-            print('Clearing particle cache...', end="")
-            shutil.rmtree(data_dir)
-            os.mkdir(data_dir)
-            print('Done.')
-        
-        os.mkdir(train_dir)
-        os.mkdir(test_dir)
-        good_train_dir = train_dir + '/good'
-        os.mkdir(good_train_dir)
-        
-        bad_train_dir = train_dir + '/bad'
-        os.mkdir(bad_train_dir)
-        
-        good_test_dir = test_dir + '/good'
-        os.mkdir(good_test_dir)
-        
-        bad_test_dir = test_dir + '/bad'
-        os.mkdir(bad_test_dir)
-        
-        # extract good and bad particle data
-        extract_particles.extract(args.projpath, args.stargood, good_train_dir, good_test_dir, 'good')
-        extract_particles.extract(args.projpath, args.starbad, bad_train_dir, bad_test_dir, 'bad')
-        
-    else:  # FOR DEBUGGING
-        "Particle cache not cleared."
-        
-    # loader parameters
+    # from star file extract x coord, y coord, particle stack name, particle stack index for each particle
+    meta = []
+    with open(args.imagestar, "r") as openfile:
+        for line in openfile:
+
+            # get column for x coordinate
+            if re.search(r'_rlnCoordinateX', line):
+                x_col = int(str(line.split()[1]).strip("#"))
+                
+            # get column for y coordinate
+            if re.search(r'_rlnCoordinateY', line):
+                y_col = int(str(line.split()[1]).strip("#"))
+                
+            # get column for particle name 
+            if re.search(r'_rlnImageName', line):
+                par_col = int(str(line.split()[1]).strip("#"))
+
+            # use keywords to find lines with micrograph names
+            if re.search(r'(mrc | mrcs | tif | tiff)', line):
+                
+                x = float(line.split()[x_col-1])
+                y = float(line.split()[y_col-1])
+                par = str(line.split()[par_col-1])
+                par_stack = str(par.split('@')[1])
+                par_idx = int(par.split('@')[0].lstrip('0')) - 1
+                meta.append([x, y, par_stack, par_idx])
+    
+    # create good and bad star file
+    star_good = star_head()
+    star_bad = star_head()
+    
+    # get box size by reading in a particle stack
+    mrc_path = work_dir + '/' + meta[0][2]
+    mrc = mrcfile.open(mrc_path, mode=u'r', permissive=False, header_only=False)
+    box = len(mrc.data[0,:,:][0])
+    mrc.close()
+    
+    # setup for iterating through star meta to get batches
+    par_cnt = len(meta)
     batch_size = 32
-    image_size = (box, box)
-    class_names=['bad', 'good']
-    train_val_split = 0.2
+    batch_num = math.floor(par_cnt/batch_size) # total number of batches (not counting the modulo)
+    batch_fragment_size = par_cnt%batch_size # get number of samples in the final batch (modulo)
 
-    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-        train_dir,
-        validation_split=train_val_split,
-        class_names=class_names,
-        label_mode='binary',
-        subset='training',
-        seed=seed,
-        image_size=image_size,
-        batch_size=batch_size,
-        color_mode='grayscale'
-    )
+    if batch_fragment_size > 0: # if final batch has fewer than 32 samples, make sure it is included in the batch count
+        batch_num += 1
 
-    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-        train_dir,
-        validation_split=train_val_split,
-        class_names=class_names,
-        label_mode='binary',
-        subset='validation',
-        seed=seed,
-        image_size=image_size,
-        batch_size=batch_size,
-        color_mode='grayscale'
-    )
+    batch_star = [] # for each batch store the lines for particle star file (32 entries)
     
-    test_ds = tf.keras.preprocessing.image_dataset_from_directory(
-        test_dir,
-        class_names=class_names,
-        label_mode='binary',
-        seed=seed,
-        image_size=image_size,
-        batch_size=batch_size,
-        color_mode='grayscale'
-    )
+    # each round of loop will predict on a batch
+    for batch_cnt in range(0,batch_num): 
+        # set metadata range for current batch
+        if ((batch_cnt == batch_num-1) and (batch_fragment_size > 0)):
+            start = batch_size*(batch_cnt)
+            end = start + batch_fragment_size
 
-    train_ds = train_ds.prefetch(buffer_size=32)
-    val_ds = val_ds.prefetch(buffer_size=32)
-    test_ds = test_ds.prefetch(buffer_size=32)
+        else:
+            start = batch_size*(batch_cnt)
+            end = start + batch_size
 
-    # build model
-    model = Sequential()
-    
-    model.add(tf.keras.layers.experimental.preprocessing.RandomRotation(0.2, seed=seed)) # augmentation
-    #model.add(tf.keras.layers.experimental.preprocessing.CenterCrop(height=round(0.8*box), width=round(0.8*box))) # augmentation
-    #model.add(tf.keras.layers.experimental.preprocessing.RandomTranslation(height_factor=(0.1), width_factor=(0.1), seed=seed)) # augmentation
-    
-    model.add(Conv2D(
-        filters=32,
-        kernel_size=(2,2),
-        strides=(1,1),
-        padding='same',
-        input_shape=(box,box,1)
-    ))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
+        batch_star = meta[start:end] # xcoord, ycoord, particle name, and slice for all particles in a batch
 
-    model.add(MaxPooling2D(pool_size=(2,2),strides=2))
-    model.add(Conv2D(filters=64,kernel_size=(2,2),strides=(1,1),padding='valid'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
+        # load mrc batch into array
+        batch_img = np.zeros(shape=(len(batch_star),box,box,1)) # for each batch store the image batch (32 entries, or fewer for final batch)
+        loader_cnt = 0
+        for sample in batch_star:
 
-    model.add(MaxPooling2D(pool_size=(2,2),strides=2))
-    model.add(Conv2D(filters=128,kernel_size=(2,2),strides=(1,1),padding='valid'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    
-    # model.add(MaxPooling2D(pool_size=(2,2),strides=2))
-#     model.add(Conv2D(filters=128,kernel_size=(2,2),strides=(1,1),padding='valid'))
-#     model.add(BatchNormalization())
-#     model.add(Activation('relu'))
+            mrc_stack = work_dir + '/' + sample[2]
+            mrc_slice = sample[3]
+            mrc = mrcfile.open(mrc_stack, mode=u'r', permissive=False, header_only=False)
+            img = np.flip(mrc.data[:, :])
+            img = np.flip(mrc.data[mrc_slice, :, :], axis=0)
+            img = img + abs(img.min()) # make all 32 bit floating point pixel values >= 0
+            img /= img.max() # normalize all pixels between 0 and 1
+            img *= 255 # normalize all pixels between 0 and 255
 
-    model.add(MaxPooling2D(pool_size=(2,2),strides=2))
-    model.add(Flatten())
-    model.add(Dense(32))
+            batch_img[loader_cnt,:,:,0] = img
+            loader_cnt += 1
 
-    model.add(Activation('relu'))
-    model.add(Dropout(0.25))
-    model.add(Dense(1))
-    model.add(Activation('sigmoid'))
-
-    model.compile(
-        loss='binary_crossentropy',
-        optimizer=keras.optimizers.Adam(0.00005),
-        metrics = [
-            keras.metrics.BinaryAccuracy(name='accuracy'),
-            keras.metrics.TruePositives(name='tp'),
-            keras.metrics.FalsePositives(name='fp'),
-            keras.metrics.TrueNegatives(name='tn'),
-            keras.metrics.FalseNegatives(name='fn'),
-            keras.metrics.Precision(name='precision'),
-            keras.metrics.Recall(name='recall'),
-            keras.metrics.AUC(name='auc')
-        ]
-    )
-    
-    # callbacks
-    checkpoint_filepath = '/tmp/checkpoint'
-    callbacks = [
-        keras.callbacks.EarlyStopping(monitor="val_loss", patience=2),
-        #keras.callbacks.ModelCheckpoint(checkpoint_filepath, save_freq='epoch'),
-        #keras.callbacks.TensorBoard(log_dir='./logs')
-    ]
-
-    if args.skiptraining != True:
-        # fit model to data and save training and validation history
-        history = model.fit( 
-            train_ds,
-            epochs=50,
-            callbacks=callbacks,
-            validation_data=val_ds
-        )
-        model.save(data_dir + '/model.h5')
-        
-        # save log of error and accuracy per epoch (dict)
-        with open(data_dir + "/training_log.txt", "w") as text_file:
-            text_file.write(json.dumps(history.history))
-    else:
-        try:
-            model = tf.keras.models.load_model(data_dir + '/model.h5')
-        except:
-            print("No model found. Must run training.")
-    
-    # run prediction with test data
-    # each batch in the test dataset is a tuple with two elements
-    # element 0 is tuple with (batch_size, box, box, 1)
-    # element 1 is tuple with (batch_size, 1)
-    batch_labels = []
-    batch_data = []
-    labels = []
-    predictions = []
-    for batch in test_ds:
-        #print(len(batch))
-        batch_data = batch[0]
-        batch_labels = batch[1]
-        batch_labels = np.array(batch_labels[:,0]) # convert tuple to array
-        #print(np.shape(batch_data))
-        #print(np.shape(batch_labels))
-
-        batch_pred = model.predict(batch_data)
+        # predict on batch
+        batch_pred = model.predict(batch_img)
         batch_pred = abs(batch_pred.round())
         batch_pred = np.array(batch_pred[:,0]) # convert tuple to array
         
-        # store batch labels and batch predictions
-        labels = np.concatenate([labels, batch_labels])
-        labels = labels.astype(int)
-        predictions = np.concatenate([predictions, batch_pred])
-        predictions = predictions.astype(int)
-        
-        # save log of labels and predictions
-        with open(data_dir + "/testing_results.txt", "w") as text_file:
-            text_file.write(json.dumps({"labels":labels.tolist(), "predictions":predictions.tolist()}))
-        
-    # make summary of training and test results (png)
-    save_results.make_summary(data_dir, history.history, labels, predictions)
-
+        # update star arrays
+        pos_idx = np.argwhere(batch_pred==1)
+        for idx in pos_idx:
+            pass
+            #star_good.append(str(format(meta[0], '.1f').rjust(10)) + ' ' + str(format(meta[1], '.1f').rjust(10)) + '            2   -999.00000   -999.00000')
+    
+def star_head():
+    
+    star = []
+    star.append('')
+    star.append('# version 30001')
+    star.append('')
+    star.append('data_')
+    star.append('')
+    star.append('loop_')
+    star.append('_rlnCoordinateX #1')
+    star.append('_rlnCoordinateY #2')
+    star.append('_rlnClassNumber #3')
+    star.append('_rlnAnglePsi #4')
+    star.append('_rlnAutopickFigureOfMerit #5')
+    
+    return star
+    
+def star_update(star, par):
+    star.append(par)
+    return star
+    
+# def star_write():
+#     return
+    
 if __name__ == "__main__":
    gen_picks()
